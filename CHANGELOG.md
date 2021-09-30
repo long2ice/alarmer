c020f707466d844d6a4ac59cbd264130283e32e5
long2ice <long2ice@gmail.com>
2021-09-30 23:03:22 +0800

    Add `level` parameter to `LoggingHandler`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## 0.1
 
+### 0.1.2
+
+- Add `level` parameter to `LoggingHandler`.
+
 ### 0.1.1
 
 - Fix throttling optional.
--- alarmer/log.py
+++ alarmer/log.py
@@ -4,6 +4,10 @@ from alarmer import Alarmer
 
 
 class LoggingHandler(logging.Handler):
+    def __init__(self, level: int = logging.ERROR):
+        super().__init__()
+        self.level = level
+
     def emit(self, record: logging.LogRecord) -> None:
-        if record.levelno >= logging.ERROR:
+        if record.levelno >= self.level:
             Alarmer.send(record.getMessage())
